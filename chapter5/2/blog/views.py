aab8ed2cc306e814d3c2a4b47c7b1afbcff1ec0e
wyqnumber <wyqnumber1@gmail.com>
2017-07-23 12:39:54 +0800

    chapter5 Finished

--- chapter5/2/blog/views.py
+++ chapter5/2/blog/views.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
 
-# Create your views here.
+from blog.models import Blog
+
+
+def blog_list(request):
+    return render_to_response('blog/list.html', {
+        'blogs': Blog.objects.all()
+    })
+
+
+def blog_detail(request, slug):
+    return render_to_response('blog/detail.html', {
+        'blog': get_object_or_404(Blog, slug=slug)
+    })
